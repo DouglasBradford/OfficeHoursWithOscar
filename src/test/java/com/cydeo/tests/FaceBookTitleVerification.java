ec326dc19befa8b8d6ec37a148232cc9155a854d
DouglasBradford <dcb281@gmail.com>
2023-01-25 12:50:11 -0500

    FaceBook file updated

--- src/test/java/com/cydeo/tests/FaceBookTitleVerification.java
+++ src/test/java/com/cydeo/tests/FaceBookTitleVerification.java
@@ -1,7 +1,29 @@
 package com.cydeo.tests;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class FaceBookTitleVerification {
     public static void main(String[] args) {
+        //TC#1 Facebook Title verification
+        //1.Open chrome browser
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
 
+        //2. Go to https://www.facebook.com
+        driver.get("https://www.facebook.com");
+
+        //3. Verify Title:
+        //expected: "Facebook - log in or sign up"
+        String expectedTitle = "Facebook - log in or sign up";
+        String actualTitle = driver.getTitle();
+
+        if (actualTitle.equals(expectedTitle)){
+            System.out.println("PASSED!");
+        }else {
+            System.out.println("FAILED!");
+        }
     }
 }
